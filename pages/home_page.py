ba415ddc0f74743b624604095496f1b88a9da558
caseynordberg1-byte <casey.nordberg1@gmail.com>
2026-05-04 15:31:11 -0600

    5/4

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,3 +1,36 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
+class HomePage(BasePage):
+    #---Locators----------------------------------------
+    SEARCH_BOX = (By.ID, "twotabsearchtextbox")
+    SEARCH_BUTTON = (By.ID, "nav-search-submit-button")
+    SEARCH_RESULTS_HEADER = (By.CSS_SELECTOR, "h2.a-size-mini")
+    FIRST_PRODUCT = (By.CSS_SELECTOR, "div[data-component-type='s-search-result'] h2 a")
+
+    #---URL---------------------------------------------
+    HOME_URL = "https://www.amazon.com"
+
+    #---Actions-----------------------------------------
+    def open_home_page(self):
+        """Navigate to homepage."""
+        self.open(self.HOME_URL)
+
+    def enter_search_term(self, search_term: str):
+        """Type search query into search box."""
+        self.type(self.SEARCH_BOX, search_term)
+
+    def click_search_button(self):
+        """Click the search button."""
+        self.click(self.SEARCH_BUTTON)
+
+    def search_product(self, product_name: str):
+        """Complete search flow in one action."""
+        self.enter_search_term(product_name)
+        self.click_search_button()
+
+    def click_first_product(self):
+        """Click the first product in search results."""
+        self.click(self.FIRST_PRODUCT)
+
+    
